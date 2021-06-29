781ae8740f6e7d171e0c0fe71d1d6d1d3d1d3dcb
Maninder2407 <74196702+Mani2407@users.noreply.github.com>
2021-06-29 09:24:07 -0400

    Add files via upload

---
+++ TravelImages (1).js
@@ -0,0 +1,16 @@
+import React from "react";
+
+export default function TravelImages() {
+  return (
+    <main>
+      <h1 class="head1">Dubai</h1>
+      <div class="three-img-container">
+        <div id="img-first" class="div-img top-img"></div>
+        <div id="img-second" class="div-img mid-img"></div>
+        <div id="img-third" class="div-img bottom-img"></div>
+
+        <div class="info-container"></div>
+      </div>
+    </main>
+  );
+}
