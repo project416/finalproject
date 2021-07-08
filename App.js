ca20faebfb5c688c58d45342d2d77a890c494e3e
japdeepsingh <79742857+japdeepsingh@users.noreply.github.com>
2021-07-08 14:54:08 -0400

    Add files via upload

--- App.js
+++ App.js
@@ -1,57 +1,53 @@
-import './App.css';
-import {
-  BrowserRouter as Router,
-  Switch,
-  Route,
-  Link,
-  useRouteMatch,
-  useParams,
-} from "react-router-dom";
-import Home from './components/Home'
-import TravelImages from './components/TravelImages';
+import "./App.css";
+import { BrowserRouter as Router, Switch, Route, Link } from "react-router-dom";
+import Home from "./components/Home";
+import TravelImages from "./components/TravelImages";
+import Locations from "./components/locations";
 
 function App() {
   return (
     <Router>
-    <>
-      <header className="App-header">
-        <Link to="/" className="head-logo">Dubai Tourism</Link>
-        <nav>
-            <ul class="nav-list">
-                <li>
-                  <Link to="/locations" className="nav-link">Locations</Link>
-                </li>
-                <li>
-                  <Link to="/travel-images" className="nav-link">Travel Images</Link>
-                </li>
+      <>
+        <header className="App-header">
+          <Link to="/" className="head-logo">
+            Dubai Tourism
+          </Link>
+          <nav>
+            <ul className="nav-list">
+              <li>
+                <Link to="/locations" className="nav-link">
+                  Locations
+                </Link>
+              </li>
+              <li>
+                <Link to="/travel-images" className="nav-link">
+                  Travel Images
+                </Link>
+              </li>
             </ul>
-        </nav>
-      </header>
+          </nav>
+        </header>
 
-      <Switch>
-        <Route path="/" exact>
-          <Home />
-        </Route>
+        <Switch>
+          <Route path="/" exact>
+            <Home />
+          </Route>
 
-        <Route path="/locations">
-          <Location/>
-        </Route>
+          <Route path="/locations">
+            <Locations />
+          </Route>
 
-        <Route path="travel-images">
-          <TravelImages/>
-        </Route>
-      </Switch>
+          <Route path="/travel-images">
+            <TravelImages />
+          </Route>
+        </Switch>
 
-      <footer>
-        <h1>Tourism site for Dubai</h1>
-      </footer>
-    </>
+        <footer>
+          <h1>Tourism site for Dubai</h1>
+        </footer>
+      </>
     </Router>
   );
-}
-
-const Location = () => {
-  return  <h1>Locations</h1>;
 }
 
 export default App;
